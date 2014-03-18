87bb7b6ca81eced95b9e5b16c0104f4d4741c946
Daniel Walters <wioota@gmail.com>
2014-03-18 22:55:42 +0800

    add files

--- testicle2.py
+++ testicle2.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
-import sys, random
+import sys
+import random
+
 
 def get_empty(shot_grid):
     indices = []
     for index, state in enumerate(shot_grid):
-        if state == "0" :
+        if state == "0":
             if find_row(index) % 2 == 0 and index % 2 == 0:
                 indices.append(index)
                 print "even", index, state
-            elif find_row(index) % 2 <> 0 and index % 2 <> 0:
+            elif find_row(index) % 2 != 0 and index % 2 != 0:
                 indices.append(index)
                 print "odd", index, state
         else:
@@ -16,10 +18,11 @@ def get_empty(shot_grid):
 
     return indices
 
+
 def ship_search(indices, shot_grid):
     #rn = random.randint(0, len(indices) - 1)
     # go through every 3rd
-    '''
+    """
     iteration = 0
     gap = 2
     row = 0
@@ -51,44 +54,58 @@ def ship_search(indices, shot_grid):
         counter = counter + gap
     
     if not shot and shot <> 0 :
-    '''
+    """
+    global shot
     if True:
         #print "hoohah!"
         shot = indices[random.randint(0, len(indices) - 1)]
     return shot
 
+
 def find_row(i):
     return abs(i / 10) + 1
 
+
 def up(i):
-    '''up'''
+    """up"""
     shot = i - 10
     if shot < 0:
         shot = False
     return shot
 
+
 def down(i):
-    '''down'''
+    """down"""
     shot = i + 10
     if shot > 99:
-        shot = False   
-    return shot 
-
-def left(i):
-    '''left'''
-    shot = i - 1
-    if find_row(shot) < find_row(i) :
         shot = False
     return shot
 
+
+def left(i):
+    """left"""
+    shot = i - 1
+    if find_row(shot) < find_row(i):
+        shot = False
+    return shot
+
+
 def right(i):
-    '''right'''
+    """right"""
     shot = i + 1
     if shot > 99:
         shot = False
     return shot
 
-def main(shot_grid = []):
+
+def main(shot_grid=None):
+    """
+
+    :param shot_grid:
+    :return:
+    """
+    if not shot_grid:
+        shot_grid = []
     if len(sys.argv) > 1:
         shot_grid = sys.argv[1]
 
@@ -106,7 +123,7 @@ def main(shot_grid = []):
                 shot = ship_kill(i, shot_grid)
                 break
 
-    if not shot :
+    if not shot:
         #print "searching...\n"
         shot = ship_search(indices, shot_grid)
 
@@ -114,12 +131,13 @@ def main(shot_grid = []):
     #print "\n"
     return str(shot)
 
-def ship_kill(i, shot_grid):   
+
+def ship_kill(i, shot_grid):
     funcs = [left, up, right, down]
     shot = False
     #search all direction
     search_list = []
-    for func in funcs :
+    for func in funcs:
         target = func(i)
         search_list.append([func.__doc__, target])
         #if we found an existing hit, try determine orientation
@@ -131,28 +149,30 @@ def ship_kill(i, shot_grid):
         else:
             pass
             #print "invalid: " + func.__doc__ + ", " + str(target) + ", " + shot_grid[target]
-                
+
         if shot:
             #print "primary: " + str(shot)
             return shot
 
     #alternatively if we cannot find any consecutive strikes select the first valid unknown
     #print "alternative"
-    for func in funcs :
+    for func in funcs:
         target = func(i)
         if target and shot_grid[target] == "0":
             shot = target
-        
+
     return shot
 
-def ship_kill_direction (i, shot_grid, funcs):
+
+def ship_kill_direction(i, shot_grid, funcs):
+    shot = None
     start = i
-    for func in funcs :
+    for func in funcs:
         i = start
         shot = False
         counter = 0
-        while not shot :
-            counter = counter + 1
+        while not shot:
+            counter += 1
             target = func(i)
             # if we find a valid shot to take
             if target and shot_grid[target] == "0":
@@ -160,23 +180,25 @@ def ship_kill_direction (i, shot_grid, funcs):
                 shot = target
                 break
             elif target and shot_grid[target] == "1":
-                i = func(i) #otherwise keep looking this direction
+                #otherwise keep looking this direction
+                i = func(i)
             elif not target or shot_grid[target] == "-1" or counter > 5:
                 break
-                
+
     return shot
+
 
 def ship_kill_horizontal(i, shot_grid):
     funcs = [left, right]
     #print "looking horizontal...\n"
     return ship_kill_direction(i, shot_grid, funcs)
-    
-    
+
+
 def ship_kill_vertical(i, shot_grid):
     funcs = [up, down]
     #print "looking vertical...\n"
     return ship_kill_direction(i, shot_grid, funcs)
-        
-            
+
+
 if __name__ == "__main__":
     main()
