bf97697d140e00b5b3938a806f48a0429b62526d
Daniel Walters <wioota@gmail.com>
2014-03-18 22:22:37 +0800

    add files

---
+++ localmanager3.py
@@ -0,0 +1,331 @@
+#import logging.config
+import sys
+import inspect
+import os
+import os.path
+import random
+import time
+import traceback
+from operator import itemgetter
+
+# Grids ------------------------------------------------------------------------
+
+class ShipGridSquareState(object):
+    SEA =                   0
+    AIRCRAFT_CARRIER =      1
+    BATTLESHIP =            2
+    SUBMARINE =             3
+    DESTROYER =             4
+    PATROL_BOAT =           5
+
+
+class ShotGridSquareState(object):
+    UNKNOWN =   0
+    MISS =      -1
+    HIT =       1
+    SUNK =      2
+
+
+class Grid(object):
+
+    SIZE = 10
+
+    def __init__(self, init_val):
+        """Populate the grid with an initial value."""
+        self.squares = [init_val] * (self.SIZE ** 2)
+
+    def get(self, x, y):
+        return self.squares[(y * self.SIZE) + x]
+
+    def put(self, x, y, val):
+        self.squares[(y * self.SIZE) + x] = val
+
+    def valid_coord(self, x, y):
+        """Return whether x,y are valid grid coordinates."""
+        return x >= 0 and x < self.SIZE and y >= 0 and y < self.SIZE
+
+    def rand_square(self):
+        """Return the coordinates of a random grid square."""
+        x = random.randint(0, self.SIZE-1)
+        y = random.randint(0, self.SIZE-1)
+        return x, y
+
+    def __str__(self):
+        """Return string representation of the grid.
+
+        Used for serialising the grid for transmission to bot script.
+        """
+        return ','.join(map(str, self.squares))
+
+    @classmethod
+    def index_to_coord(cls, i):
+        y = i / cls.SIZE
+        x = i % cls.SIZE
+        return x, y
+
+
+class ShipGrid(Grid):
+
+    def get_ship_squares(self, ship_type):
+        """Generate a list of index positions for the squares that contain the
+        ship `ship_type`.
+        """
+        for i, val in enumerate(self.squares):
+            if val == ship_type:
+                yield i
+
+
+# Ship Manager -----------------------------------------------------------------
+
+class ShipManager(object):
+
+    # (ship_type, ship_size)
+    SHIPS = [
+        (ShipGridSquareState.AIRCRAFT_CARRIER,  5),
+        (ShipGridSquareState.BATTLESHIP,        4),
+        (ShipGridSquareState.SUBMARINE,         3),
+        (ShipGridSquareState.DESTROYER,         3),
+        (ShipGridSquareState.PATROL_BOAT,       2),
+        ]
+
+    @classmethod
+    def arrange_on_grid(cls, ship_grid):
+        """Randomly arrange the ships `SHIPS` in the grid `ship_grid`.
+
+        To place a ship, first a random start square in the grid is picked, 
+        then a random orientation is picked (horizontal, vertical). The 
+        sequence of coordinates in the grid that would occupy the ship given 
+        the random start square and orientation is then identified. An attempt 
+        to place the ship in this sequence of coordinates is then made. If the 
+        attempt fails (see `attempt_to_place_ship_in_seq' for possible reasons) 
+        then start again by picking a new random start square.
+
+        Finish when all ships have been successfully placed.
+        """
+
+        ships = list(cls.SHIPS)
+        random.shuffle(ships)
+        for ship_type, ship_size in ships:
+            while True:
+                r_x, r_y = ship_grid.rand_square()
+                if random.choice([True, False]):
+                    # vertical
+                    seq = [(x, r_y) for x in range(r_x, r_x + ship_size)]
+                else:
+                    # horizontal
+                    seq = [(r_x, y) for y in range(r_y, r_y + ship_size)]
+                success = \
+                    cls._attempt_to_place_ship_in_seq(
+                        ship_grid, ship_type, ship_size, seq)
+                if success:
+                    break
+
+    @staticmethod
+    def _attempt_to_place_ship_in_seq(ship_grid, ship_type, ship_size, seq):
+        """Attempt to place the ship of type `ship_type` with length 
+        `ship_size` in the grid `ship_grid` in the squares identified by the 
+        list of coordinates `seq`.
+
+        The attempt will fail if:
+            * Any coordinates are invalid for the grid
+            * A square identified by a coordinate already contains a ship part
+
+        Returns whether the ship was successfully placed.
+        """
+
+        for x, y in seq:
+            if not ship_grid.valid_coord(x, y) or \
+                ship_grid.get(x, y) != ShipGridSquareState.SEA:
+                return False
+
+        for x, y in seq:
+            ship_grid.put(x, y, ship_type)
+        return True
+
+
+# Game Manager -----------------------------------------------------------------
+
+class GameManager(object):
+
+    _BOT_MOVE_TIMEOUT = 10 # max secs a bot can take to make a move
+
+    @classmethod
+    def play(cls, bot, seed=None):
+        """Generate a random ship arragement and play the game until the bot
+        has hit/sunk all ships. The number of moves taken by the bot is
+        returned. To make the game deterministic a seed can be provided for
+        the random number generator.
+
+        A BotIllegalMoveException is raised if the bot attempts to play an
+        illegal move.
+
+        Returns a summary of the game.
+
+            `ships`: a list of grid squares indicating which are occupied by
+                ships (1) and which are just sea (0)
+
+            `moves`: a list of moves made by the bot and their outcome, of the
+                form [(move_grid_index, hit_or_miss), ...] eg:
+
+                    [(3, -1), (13, 1), (14, 1), (39, -1), ...]
+
+        """
+
+        if seed is None:
+            seed = random.random()
+        random.seed(seed)
+
+        # init game state
+        ship_grid = ShipGrid(ShipGridSquareState.SEA)
+        shot_grid = Grid(ShotGridSquareState.UNKNOWN)
+        ShipManager.arrange_on_grid(ship_grid)
+
+        # repeatedly ask the bot to play moves until all ships are hit/sunk
+        try:
+            moves = []
+            while not cls._all_ships_hit(shot_grid):
+                move = cls._play_next_bot_move(bot, ship_grid, shot_grid)
+                moves.append(move)
+            return {
+                "success":      True,
+                "moves":        moves,
+                "ships":        ship_grid.squares,
+                }
+
+        except _BotMoveIllegalException as e:
+            data = e.data.copy()
+            data.update({
+                "success":      False,
+                "error":        "Bot made an illegal move",
+                "seed":         seed,
+                })
+            return data
+
+        except _BotMoveTimeoutException:
+            return {
+                "success":      False,
+                "error":        "Bot timeout",
+                "seed":         seed,
+                }
+
+        except _BotErrorException:
+            return {
+                "success":      False,
+                "error":        "Bot syntax error",
+                "seed":         seed,
+                }
+
+    @staticmethod
+    def _all_ships_hit(shot_grid):
+        """Return whether all ships have been hit/sunk.
+
+        This is calculated by comparing the number of hits in the shot grid 
+        `hits_made` with the number of squares occupied by ships in the ship 
+        grid `hits_to_win`.
+        """
+        hits_made = len(filter(
+            lambda x: x == ShotGridSquareState.SUNK, shot_grid.squares))
+        # index 1 of `SHIPS` is the ship size
+        hits_to_win = sum(map(itemgetter(1), ShipManager.SHIPS))
+        return hits_made == hits_to_win
+
+    @classmethod
+    def _play_next_bot_move(cls, bot, ship_grid, shot_grid):
+        """Run the bot script against the current `shot_grid` to obtain the
+        next move, then attempt to update the `shot_grid` by playing the move.
+        """
+
+        bot_move = bot.main(str(shot_grid))
+        # validate the bot move
+        try:
+            bot_move = int(bot_move)
+        except ValueError:
+            print "\n", str(shot_grid), str(bot_move), "\n"
+            raise _BotMoveIllegalException({
+                "game_state":   str(shot_grid),
+                "move":         str(bot_move),
+                })
+        if bot_move < 0 or bot_move >= len(shot_grid.squares):
+            print "\n", str(shot_grid), str(bot_move), "\n"
+            raise _BotMoveIllegalException({
+
+                "game_state":   str(shot_grid),
+                "move":         bot_move,
+                })
+        x, y = Grid.index_to_coord(bot_move)
+        if shot_grid.get(x, y) != ShotGridSquareState.UNKNOWN:
+            print "\n", str(shot_grid), str(bot_move), "\n"
+            raise _BotMoveIllegalException({
+                "game_state":   str(shot_grid),
+                "move":         bot_move,
+                })
+
+        # make bot move
+        square_revealed = ship_grid.get(x, y)
+
+        # miss
+        if square_revealed == ShipGridSquareState.SEA:
+            move_result = ShotGridSquareState.MISS
+            shot_grid.put(x, y, ShotGridSquareState.MISS)
+
+        # hit (maybe sunk)
+        else:
+            move_result = ShotGridSquareState.HIT
+            shot_grid.put(x, y, ShotGridSquareState.HIT)
+
+            ship_type = square_revealed
+            is_sunk = True
+            for i in ship_grid.get_ship_squares(ship_type):
+                if shot_grid.squares[i] == ShotGridSquareState.UNKNOWN:
+                    is_sunk = False
+                    break
+                    
+            if is_sunk:
+                move_result = ShotGridSquareState.SUNK
+                for i in ship_grid.get_ship_squares(ship_type):
+                    shot_grid.squares[i] = ShotGridSquareState.SUNK
+                 
+        # return move summary
+        return (bot_move, move_result)
+
+
+class _BotException(Exception):
+    """Abstract superclass for all bot exception."""
+
+    def __init__(self, data):
+        data["type"] = self.__class__.__name__
+        self.data = data
+        self.out(self.data)
+
+    def __str__(self):
+        repr(self.data)
+
+    def out(self, data):
+        print self.__str__()
+
+class _BotMoveIllegalException(_BotException): 
+    """The bot made an illegal move."""
+    pass
+
+
+class _BotMoveTimeoutException(_BotException): 
+    """The bot took too long to make a move."""
+    pass
+
+
+class _BotErrorException(_BotException):
+    """The bot encountered an error during execution."""
+    pass
+    
+# Main -------------------------------------------------------------------------
+def main():
+    module = "testicle2"
+    bot_path = os.path.join(os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda _: None))), module + ".py")
+    bot = __import__(module)
+    summary = GameManager.play(bot)
+    for k, v in summary.items():
+        print k, len(v)
+    sys.stdout.write(str(summary))
+
+if __name__ == "__main__":
+    main()
---
+++ testicle2.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python
+import sys, random
+
+def get_empty(shot_grid):
+    indices = []
+    for index, state in enumerate(shot_grid):
+        if state == "0" :
+            if find_row(index) % 2 == 0 and index % 2 == 0:
+                indices.append(index)
+                print "even", index, state
+            elif find_row(index) % 2 <> 0 and index % 2 <> 0:
+                indices.append(index)
+                print "odd", index, state
+        else:
+            pass
+
+    return indices
+
+def ship_search(indices, shot_grid):
+    #rn = random.randint(0, len(indices) - 1)
+    # go through every 3rd
+    '''
+    iteration = 0
+    gap = 2
+    row = 0
+    counter = 0
+    shot = None
+    while True:
+        # the number of iters
+        #c 0 3  6  9  1 4  7  10 2 
+        #r 0 3  6  9  0 3  6  9  0
+        #s 0 33 66 99 1 34 67 *  2
+        shot = counter + row * 10
+        #print "iterations: ", iteration,"shot: ", shot, "row: ", row, "cnt: ", counter
+
+        if shot > 99:
+            iteration = iteration + 1
+            counter = iteration + iteration * gap
+            row = 0
+            continue
+
+        if iteration > 3:
+            shot = None
+            break
+
+        if shot_grid[shot] == "0" :
+            break
+
+        # move to the new position
+        row = row + gap
+        counter = counter + gap
+    
+    if not shot and shot <> 0 :
+    '''
+    if True:
+        #print "hoohah!"
+        shot = indices[random.randint(0, len(indices) - 1)]
+    return shot
+
+def find_row(i):
+    return abs(i / 10) + 1
+
+def up(i):
+    '''up'''
+    shot = i - 10
+    if shot < 0:
+        shot = False
+    return shot
+
+def down(i):
+    '''down'''
+    shot = i + 10
+    if shot > 99:
+        shot = False   
+    return shot 
+
+def left(i):
+    '''left'''
+    shot = i - 1
+    if find_row(shot) < find_row(i) :
+        shot = False
+    return shot
+
+def right(i):
+    '''right'''
+    shot = i + 1
+    if shot > 99:
+        shot = False
+    return shot
+
+def main(shot_grid = []):
+    if len(sys.argv) > 1:
+        shot_grid = sys.argv[1]
+
+    if shot_grid:
+        shot_grid = shot_grid.split(',')
+
+    #do we still have places to shoot at?
+    indices = get_empty(shot_grid)
+
+    shot = False
+    if indices and len(indices):
+        # go through shot grid and find a hit on a ship not sunk
+        for i, state in enumerate(shot_grid):
+            if state == "1":
+                shot = ship_kill(i, shot_grid)
+                break
+
+    if not shot :
+        #print "searching...\n"
+        shot = ship_search(indices, shot_grid)
+
+    sys.stdout.write(str(shot))
+    #print "\n"
+    return str(shot)
+
+def ship_kill(i, shot_grid):   
+    funcs = [left, up, right, down]
+    shot = False
+    #search all direction
+    search_list = []
+    for func in funcs :
+        target = func(i)
+        search_list.append([func.__doc__, target])
+        #if we found an existing hit, try determine orientation
+        if target and shot_grid[target] == "1":
+            if func.__doc__ == "left" or func.__doc__ == "right":
+                shot = ship_kill_horizontal(target, shot_grid)
+            elif func.__doc__ == "up" or func.__doc__ == "down":
+                shot = ship_kill_vertical(target, shot_grid)
+        else:
+            pass
+            #print "invalid: " + func.__doc__ + ", " + str(target) + ", " + shot_grid[target]
+                
+        if shot:
+            #print "primary: " + str(shot)
+            return shot
+
+    #alternatively if we cannot find any consecutive strikes select the first valid unknown
+    #print "alternative"
+    for func in funcs :
+        target = func(i)
+        if target and shot_grid[target] == "0":
+            shot = target
+        
+    return shot
+
+def ship_kill_direction (i, shot_grid, funcs):
+    start = i
+    for func in funcs :
+        i = start
+        shot = False
+        counter = 0
+        while not shot :
+            counter = counter + 1
+            target = func(i)
+            # if we find a valid shot to take
+            if target and shot_grid[target] == "0":
+                #print "shoot: " + func.__doc__ + " " + str(target)
+                shot = target
+                break
+            elif target and shot_grid[target] == "1":
+                i = func(i) #otherwise keep looking this direction
+            elif not target or shot_grid[target] == "-1" or counter > 5:
+                break
+                
+    return shot
+
+def ship_kill_horizontal(i, shot_grid):
+    funcs = [left, right]
+    #print "looking horizontal...\n"
+    return ship_kill_direction(i, shot_grid, funcs)
+    
+    
+def ship_kill_vertical(i, shot_grid):
+    funcs = [up, down]
+    #print "looking vertical...\n"
+    return ship_kill_direction(i, shot_grid, funcs)
+        
+            
+if __name__ == "__main__":
+    main()
